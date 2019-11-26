61b69700926f8ab97412c92c9bb8dcebc54e22f5
daanwerf <daanvanderwerf@hotmail.com>
2019-11-26 14:32:13 +0100

    create registry in main method

--- Assignment 1/src/Assignment2/DA_SuzukiKazami_main.java
+++ Assignment 1/src/Assignment2/DA_SuzukiKazami_main.java
@@ -1,4 +1,13 @@
 package Assignment2;
 
+import java.rmi.RemoteException;
+
 public class DA_SuzukiKazami_main {
+    public static void main(String args[]) {
+        try {
+            java.rmi.registry.LocateRegistry.createRegistry((1099));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
